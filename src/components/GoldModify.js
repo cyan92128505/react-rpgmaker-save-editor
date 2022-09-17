d9a209dd3bfcda3cd9eb87d71e27d7aeba956146
cyan92128505 <cyan92128505@gmail.com>
2022-09-17 15:48:48 +0800

    fix: number cannot to modify in GoldModify

--- src/components/GoldModify.js
+++ src/components/GoldModify.js
@@ -11,7 +11,11 @@ import { CheckIcon } from "@chakra-ui/icons";
 const GoldModify = ({ save = {}, onSave = () => {} }) => {
   const [gold, setGold] = useState(0);
 
-  const _hundleChange = () => {
+  const _handleChange = async (event) => {
+    setGold(event.target.value);
+  };
+
+  const _hundleSave = () => {
     if (save["party"] && save["party"]["_gold"]) {
       const _target = save;
       _target["party"]["_gold"] = gold;
@@ -20,7 +24,6 @@ const GoldModify = ({ save = {}, onSave = () => {} }) => {
   };
 
   useEffect(() => {
-    console.log(save["party"]);
     if (save["party"] && save["party"]["_gold"]) {
       setGold(save["party"]["_gold"]);
     }
@@ -29,13 +32,18 @@ const GoldModify = ({ save = {}, onSave = () => {} }) => {
   return (
     <InputGroup>
       <InputLeftAddon children="GOLD" />
-      <Input type="tel" placeholder="phone number" value={gold} />
+      <Input
+        type="text"
+        placeholder="GOLD"
+        value={gold}
+        onChange={_handleChange}
+      />
       <InputRightElement width="4.5rem">
         <IconButton
           colorScheme="gray"
           size="sm"
           icon={<CheckIcon></CheckIcon>}
-          onClick={_hundleChange}
+          onClick={_hundleSave}
         ></IconButton>
       </InputRightElement>
     </InputGroup>
